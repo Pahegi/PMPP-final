f5141b7be978f9fb75b95262a87ea0a64307d312
Paul <paul.hermann@stud.tu-darmstadt.de>
2024-11-20 09:52:01 +0100

    initial commit

---
+++ waveform_evolution.cu
@@ -0,0 +1,20 @@
+#include <waveform_evolution.hpp>
+
+cuda::std::pair<pmpp::cuda_ptr<std::uint64_t[]>, std::size_t> evolve_operator(
+	cuda::std::span<std::uint64_t const> device_wavefunction,
+	std::uint64_t activation, std::uint64_t deactivation
+)
+{
+	/* TODO */
+	return {nullptr, 0};
+}
+
+cuda::std::pair<pmpp::cuda_ptr<std::uint64_t[]>, std::size_t> evolve_ansatz(
+	cuda::std::span<std::uint64_t const> device_wavefunction,
+	cuda::std::span<std::uint64_t const> activations,
+	cuda::std::span<std::uint64_t const> deactivations
+)
+{
+	/* TODO */
+	return {nullptr, 0};
+}
