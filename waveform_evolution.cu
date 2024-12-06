a9ff9eee3f0dc09e97acec3c40e8b1f8e82d04a4
ronin <taeubert@protonmail.com>
2024-12-06 15:07:03 +0100

    Semi-parallel + passed

--- waveform_evolution.cu
+++ waveform_evolution.cu
@@ -14,11 +14,38 @@ __global__ void evolve_operator_kernel(
 )
 {	
 
-	size_t output_elements = *size;
+	// size_t output_elements = *size;
 	auto op_ad = activation ^ deactivation;
 
-	size_t idx = 0;
+	size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
 
+	if(idx>=size[0]) return;
+
+	auto wave = device_wavefunction[idx];
+	auto check_deactivation = deactivation & wave; // TODO all bits or only one bit enough for deactivation?
+	auto check_activation = activation & (~wave);
+	uint64_t activate = check_activation == activation;
+	uint64_t deactivate = check_deactivation == deactivation;
+	auto op = (activate & deactivate) * op_ad;
+	auto wave_new = op ^ wave;
+
+			// insert new wavefunction, if wave_new != wave
+	
+	if (wave_new != wave) {
+		// wave_out[idx] = wave_new;
+		wave_out[idx+size[0]] = wave_new;
+
+		// output_elements++;
+	}
+	else {
+		wave_out[idx+size[0]] = 0;
+	}
+
+	//*size = output_elements;
+
+
+
+	/*
 	// only thread 0 (sequential implementation test), MASSIVELY PARALLEL!!!
 	if (threadIdx.x == 0 && blockIdx.x == 0) {
 
@@ -41,15 +68,19 @@ __global__ void evolve_operator_kernel(
 		*size = output_elements;
 
 		printf("Block: %d Thread: %d Output Elements: %lu\n", threadIdx.x, blockIdx.x, output_elements);
-
+	
 	}
+	*/
 }
 
 cuda::std::pair<pmpp::cuda_ptr<std::uint64_t[]>, std::size_t> evolve_operator(
 	cuda::std::span<std::uint64_t const> device_wavefunction,
 	std::uint64_t activation, std::uint64_t deactivation
 )
-{
+{	
+	// shift variable
+	size_t shift = 0;
+
 	// create output array double the size of input
 	printf("evolve_operator: Creating Output Array\n");
 	auto wave_out = pmpp::make_managed_cuda_array<std::uint64_t>(2 * size(device_wavefunction));
@@ -79,11 +110,11 @@ cuda::std::pair<pmpp::cuda_ptr<std::uint64_t[]>, std::size_t> evolve_operator(
 	
 	cudaDeviceSynchronize();
 
-	thrust::sort(wave_out_span.data(), wave_out_span.data()+num_elements[0]);
+	thrust::sort(wave_out_span.data(), wave_out_span.data()+2*num_elements[0]);
 
 	cudaDeviceSynchronize();
 
-	size_t *new_end = thrust::unique(wave_out_span.data(), wave_out_span.data()+num_elements[0]);
+	size_t *new_end = thrust::unique(wave_out_span.data(), wave_out_span.data()+2*num_elements[0]);
 
 	for(size_t i=0; i<num_elements[0]; i++){
 		printf("%lu ", wave_out_span[i]);
@@ -92,9 +123,14 @@ cuda::std::pair<pmpp::cuda_ptr<std::uint64_t[]>, std::size_t> evolve_operator(
 
 	cudaDeviceSynchronize();
 
+	if(wave_out_span.data()[0]==0) {
+		shift = 1;
+	}
+
+
 	printf("Num elements %lu\n", num_elements[0]);
 	printf("evolve_operator: Returning output array\n");
-	cuda::std::pair<pmpp::cuda_ptr<std::uint64_t[]>, std::size_t> result {wave_out_span.data(), (new_end - wave_out_span.data())};
+	cuda::std::pair<pmpp::cuda_ptr<std::uint64_t[]>, std::size_t> result {&(wave_out_span.data()[shift]), (new_end - wave_out_span.data())-shift};
 	return result;
 
 
