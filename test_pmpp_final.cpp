e744daab3934a376d108005b84312e375167ed22
Paul <paul.hermann@stud.tu-darmstadt.de>
2024-12-09 16:36:08 +0100

    removed even more prints

--- test_pmpp_final.cpp
+++ test_pmpp_final.cpp
@@ -19,17 +19,13 @@ std::vector<std::uint64_t> evolve_operator_host(
 
 	auto t0 = std::chrono::system_clock::now();
 
-	printf("evolve_operator_host: Allocating managed cuda array for %lu wavefunctions\n", host_wavefunction.size());
 	auto device_wavefunction_ptr = pmpp::make_managed_cuda_array<std::uint64_t>(host_wavefunction.size());
 	auto device_wavefunction = cuda::std::span(device_wavefunction_ptr.get(), host_wavefunction.size());
 
-	printf("evolve_operator_host: Copying %lu host wavefunction to device\n", host_wavefunction.size());
 	std::copy_n(data(host_wavefunction), size(host_wavefunction), device_wavefunction.data());
 
-	printf("evolve_operator_host: Calling Evolve operator\n");
 	auto [result_wavefunction, result_size] = evolve_operator(device_wavefunction, activation, deactivation);
 
-	printf("evolve_operator_host: Copying %lu results back to host\n", result_size);
 	std::vector<std::uint64_t> result(result_size);
 	if (result_size)
 		cudaMemcpy(data(result), result_wavefunction.get(), sizeof(std::uint64_t) * result_size, cudaMemcpyDefault);
@@ -46,7 +42,8 @@ std::vector<std::uint64_t> evolve_ansatz_host(
 	using std::data;
 	using std::size;
 
-	printf("evolve_ansatz_host: Allocating managed cuda array for %lu wavefunctions\n", host_wavefunction.size());
+	auto t0 = std::chrono::system_clock::now();
+
 	auto device_wavefunction_ptr = pmpp::make_managed_cuda_array<std::uint64_t>(size(host_wavefunction));
 	auto device_wavefunction = cuda::std::span(device_wavefunction_ptr.get(), size(host_wavefunction));
 	std::copy_n(data(host_wavefunction), size(host_wavefunction), device_wavefunction.data());
@@ -64,6 +61,9 @@ std::vector<std::uint64_t> evolve_ansatz_host(
 	std::vector<std::uint64_t> result(result_size);
 	if (result_size)
 		cudaMemcpy(data(result), result_wavefunction.get(), sizeof(std::uint64_t) * result_size, cudaMemcpyDefault);
+
+	auto t1 = std::chrono::system_clock::now();
+	std::printf("Time: %ld µs\n", std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count());
 
 	return result;
 }
@@ -140,14 +140,6 @@ TEST_CASE("Test evolve operator", "[simple]") {
 							 std::span<std::uint64_t const> wfn_out,
 							 std::uint64_t activation,
 							 std::uint64_t deactivation) {
-		// print wfn_in!!!
-		printf("test_evolve_operator: elements of wfn_in: ");
-		for (auto i : wfn_in) {
-			std::cout << i << " ";
-		}
-		printf("\n");
-		printf("test_evolve_operator: size of wfn_in: %lu\n", wfn_in.size());
-		printf("test_evolve_operator: size of wfn_out: %lu\n", wfn_out.size());
 		auto wfn_out_dut = evolve_operator_host(wfn_in, activation, deactivation);
 		auto wfn_out_set = std::unordered_set(begin(wfn_out), end(wfn_out));
 		auto wfn_out_dut_set = std::unordered_set(begin(wfn_out_dut), end(wfn_out_dut));
@@ -156,36 +148,13 @@ TEST_CASE("Test evolve operator", "[simple]") {
 	});
 }
 
-// TEST_CASE("Custom Test evolve operator, "[simple]") {
-// 	using std::begin;
-// 	using std::end;
-
-// 	std::vector<std::uint64_t> wfn_in = {1, 2, 3, 4, 5, 6, 7, 8};
-// 	std::uint64_t activation = 1;
-// 	std::uint64_t deactivation = 2;
-// 	auto wfn_out = evolve_operator_host(wfn_in, activation, deactivation);
-// 	printf("test_evolve_operator: size of wfn_out: %lu\n", wfn_out.size());
-// 	printf("test_evolve_operator: elements of wfn_out: ");
-// 	for (auto i : wfn_out) {
-// 		std::cout << i << " ";
-// 	}
-// 	printf("\n");
-// }
-
 TEST_CASE("Test evolve ansatz", "[simple]") {
 	using std::begin;
 	using std::end;
 
 	test_data_loader loader("example_evolution.bin");
 	auto [wfn_in, wfn_out] = loader.first_and_last_wavefunction();
-	printf("test_evolve_ansatz: size of wfn_in: %lu\n", wfn_in.size());
 	auto wfn_out_dut = evolve_ansatz_host(wfn_in, loader.activations(), loader.deactivations());
-	printf("test_evolve_ansatz: size of wfn_out: %lu\n", wfn_out_dut.size());
-	printf("test_evolve_ansatz: elements of wfn_out: ");
-	for (auto i : wfn_out_dut) {
-		std::cout << i << " ";
-	}
-	printf("\n");
 	auto wfn_out_set = std::unordered_set(begin(wfn_out), end(wfn_out));
 	auto wfn_out_dut_set = std::unordered_set(begin(wfn_out_dut), end(wfn_out_dut));
 	REQUIRE(wfn_out_dut.size() == wfn_out_dut_set.size());
