d5106d0b4418d3c93248482372bd882caa5a68f7
paul.hermann <paul.hermann@stud.tu-darmstadt.de>
2024-12-14 13:19:25 +0100

    add plot of sizes

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -9,35 +9,64 @@ def main():
 	for file in os.listdir('csv'):
 		if file.endswith('.csv'):
 			name = file.split('.')[0]
-			output = 'pdf/' + name + '.pdf'
 			with open('csv/' + file, 'r') as f:
 				reader = csv.reader(f)
 				data = list(reader)
-				plot_data(data, output)
+				plot_times(data, 'pdf/' + name + '_times.pdf')
+				plot_sizes(data, 'pdf/' + name + '_sizes.pdf')
 
-
-def plot_data(data, output_file):
+def split_data_for_electrons(data):
     # Ignore the first row (header)
     header = data[0]
     data = data[1:]
 
-    # Extract unique values from the second column (number of inputs)
+    # get the unique values of the second column
     unique_inputs = sorted(set(row[1] for row in data))
 
-    # Create a plot for each unique value in the second column
-    for input_value in unique_inputs:
-        x = [int(row[0]) for row in data if row[1] == input_value]
-        y = [float(row[2])/1000 for row in data if row[1] == input_value]
+    # return a list of lists, each list containing the rows with the same second column value
+    return [[row for row in data if row[1] == input_value] for input_value in unique_inputs]
+
+def plot_times(data, output_file):
+    electrons = split_data_for_electrons(data)
+
+    # plot a line for each electron
+    for electron in electrons:
+        x = [int(row[0]) for row in electron]
+        y = [float(row[2])/1000 for row in electron]
         line, = plt.plot(x, y)
         # Add a label to the beginning of the line
-        plt.annotate(f'{input_value}', xy=(x[0], y[0]), textcoords='offset points', xytext=(5,5), ha='right', color=line.get_color())
+        plt.annotate(f'{electron[0][1]}', xy=(x[0], y[0]), textcoords='offset points', xytext=(5,5), ha='right', color=line.get_color())
 
     plt.xlabel('Number of operators')
     plt.ylabel('Time (ms)')
     plt.savefig(output_file)
     plt.close()
 
+def plot_sizes(data, output_file):
+    # Ignore the first row (header)
+    data = data[1:]
 
+    # Check if each line has 4 columns
+    for row in data:
+        if len(row) != 4:
+            print('Skipping file, invalid number of columns')
+            return
+
+    electrons = split_data_for_electrons(data)
+
+    # plot a line for each electron
+    for electron in electrons:
+        x = [int(row[0]) for row in electron]
+        y = [float(row[3]) for row in electron]
+        line, = plt.plot(x, y)
+        # Add a label to the beginning of the line
+        plt.annotate(f'{electron[0][1]}', xy=(x[0], y[0]), textcoords='offset points', xytext=(5, 5), ha='right',
+                     color=line.get_color())
+
+    plt.xlabel('Number of operators')
+    plt.ylabel('waveform size')
+    plt.savefig(output_file)
+    plt.close()
 
 
 
